8d889e367bcf1f9987a7508b41e071bbd79075e2
kevinta893 <kevinta893@gmail.com>
2020-03-24 22:41:06 +0900

    Set default lighting mode for default keymap to animated rainbow

--- keyboards/koolertron/amag23/keymaps/default/keymap.c
+++ keyboards/koolertron/amag23/keymaps/default/keymap.c
@@ -30,6 +30,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 };
 
+void keyboard_post_init_user(void) {
+  rgb_matrix_enable_noeeprom();
+  rgb_matrix_mode_noeeprom(RGB_MATRIX_RAINBOW_MOVING_CHEVRON);
+}
+
 /*
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     return true;
