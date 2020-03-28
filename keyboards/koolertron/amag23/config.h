2155cf08f7f470734d97dac5d591a4cb4d38f20b
kevinta893 <kevinta893@gmail.com>
2020-03-28 09:42:08 +0900

    Fix description in config.h so that the CONSOLE_ENABLE compiles

--- keyboards/koolertron/amag23/config.h
+++ keyboards/koolertron/amag23/config.h
@@ -24,7 +24,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DEVICE_VER 0x0001
 #define MANUFACTURER Koolertron
 #define PRODUCT AMAG23
-#define DESCRIPTION A 23 key, RGB backlit mechanical keyboard
+#define DESCRIPTION "A 23 key, RGB backlit mechanical keyboard"
 
 #define MATRIX_ROWS 6
 #define MATRIX_COLS 4
