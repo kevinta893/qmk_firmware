eaa5ac582ae119815e7b303a804c698db258a343
kevinta893 <kevinta893@gmail.com>
2020-04-20 16:56:46 +0900

    Add fidget layer to personal keymap

--- keyboards/koolertron/amag23/keymaps/kevinta893/keymap.c
+++ keyboards/koolertron/amag23/keymaps/kevinta893/keymap.c
@@ -17,35 +17,43 @@
 
 // Macro/Custom keycodes
 enum custom_keycodes {
-    SWITCH_TO_NEXT_LAYER,
+    SWITCH_TO_NEXT_LAYER = SAFE_RANGE,
 };
 
 // Layers for keycodes and keymaps
 enum layer_names {
     _PROGRAMMING,
     _GAMING,
+    _FIDGET
 };
 
 // Layers in order of next layer order
-#define LAYER_COUNT 2
+#define LAYER_COUNT 3
 const uint8_t layer_list_user[LAYER_COUNT] = {
     _PROGRAMMING,
-    _GAMING
+    _GAMING,
+    _FIDGET
 };
 
 // Key Maps
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_PROGRAMMING] = LAYOUT(
         KC_ESCAPE,              LCTL(KC_X),         LCTL(KC_C),             LCTL(KC_V),             RGB_TOG,            SWITCH_TO_NEXT_LAYER,
-        LGUI(LCTL(KC_F4)),      LGUI(KC_TAB),       LGUI(LCTL(KC_D)),       KC_R,                   KC_R,               KC_R,
-        LGUI(LCTL(KC_LEFT)),    KC_AUDIO_VOL_UP,    LGUI(LCTL(KC_RIGHT)),   KC_D,                   KC_R,               KC_R,
+        KC_NO,                  LGUI(KC_TAB),       LGUI(LCTL(KC_D)),       KC_NO,                  KC_NO,              KC_NO,
+        LGUI(LCTL(KC_LEFT)),    KC_AUDIO_VOL_UP,    LGUI(LCTL(KC_RIGHT)),   KC_NO,                  KC_NO,              KC_NO,
         KC_MEDIA_NEXT_TRACK,    KC_AUDIO_VOL_DOWN,  KC_MEDIA_NEXT_TRACK,    KC_MEDIA_PLAY_PAUSE,    KC_NO,              KC_ENTER
     ),
     [_GAMING] = LAYOUT(
         KC_ESCAPE,              LCTL(LSFT(KC_F1)),  LCTL(LSFT(KC_F2)),      LCTL(LSFT(KC_F3)),      RGB_TOG,            SWITCH_TO_NEXT_LAYER,
-        KC_R,                   LALT(KC_F10),       KC_R,                   LALT(KC_F9),            LSFT(KC_TAB),       KC_R,
-        KC_R,                   KC_AUDIO_VOL_UP,    KC_R,                   KC_D,                   KC_F12,             KC_R,
+        KC_NO,                  LALT(KC_F10),       KC_NO,                  LALT(KC_F9),            LSFT(KC_TAB),       KC_NO,
+        KC_NO,                  KC_AUDIO_VOL_UP,    KC_NO,                  KC_NO,                  KC_F12,             KC_NO,
         KC_MEDIA_PREV_TRACK,    KC_AUDIO_VOL_DOWN,  KC_MEDIA_NEXT_TRACK,    KC_MEDIA_PLAY_PAUSE,    KC_NO,              LALT(KC_F1)
+    ),
+    [_FIDGET] = LAYOUT(
+        KC_NO,      KC_NO,      KC_NO,      KC_NO,      RGB_TOG,    SWITCH_TO_NEXT_LAYER,
+        KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,
+        KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,
+        KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO
     )
 };
 
@@ -61,11 +69,13 @@ void go_to_key_layer_user(uint8_t go_to_layer){
     // Setup layer
     switch (go_to_layer) {
         case _PROGRAMMING:
-            rgblight_setrgb(154, 0, 0);
-            rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
+            rgblight_mode_noeeprom(RGBLIGHT_MODE_RAINBOW_MOOD);
             break;
         case _GAMING:
             rgblight_mode_noeeprom(RGBLIGHT_MODE_RAINBOW_SWIRL + 5);
+            break;
+        case _FIDGET:
+            rgblight_mode_noeeprom(RGBLIGHT_MODE_CHRISTMAS);
             break;
     }
 
