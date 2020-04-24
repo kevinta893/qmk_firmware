86a8882411087ccc316fda21e0eaf950e70c3d5a
kevinta893 <kevinta893@gmail.com>
2020-04-24 21:36:41 +0900

    Add burnout keymap that turns off all lighting features

---
+++ keyboards/koolertron/amag23/keymaps/kevinta893burnout/keymap.c
@@ -0,0 +1,105 @@
+/* Copyright 2020 Kevin Ta
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include QMK_KEYBOARD_H
+
+// Macro/Custom keycodes
+enum custom_keycodes {
+    SWITCH_TO_NEXT_LAYER = SAFE_RANGE,
+};
+
+// Layers for keycodes and keymaps
+enum layer_names {
+    _PROGRAMMING,
+    _FIDGET
+};
+
+// Layers in order of next layer order
+#define LAYER_COUNT 2
+const uint8_t layer_list_user[LAYER_COUNT] = {
+    _PROGRAMMING,
+    _FIDGET
+};
+
+// Key Maps
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [_PROGRAMMING] = LAYOUT(
+        KC_ESCAPE,              LCTL(KC_X),         LCTL(KC_C),             LCTL(KC_V),             KC_NO,              SWITCH_TO_NEXT_LAYER,
+        KC_NO,                  LGUI(KC_TAB),       LGUI(LCTL(KC_D)),       KC_NO,                  KC_NO,              KC_NO,
+        LGUI(LCTL(KC_LEFT)),    KC_AUDIO_VOL_UP,    LGUI(LCTL(KC_RIGHT)),   KC_NO,                  KC_NO,              KC_NO,
+        KC_MEDIA_NEXT_TRACK,    KC_AUDIO_VOL_DOWN,  KC_MEDIA_NEXT_TRACK,    KC_MEDIA_PLAY_PAUSE,    KC_NO,              KC_ENTER
+    ),
+    [_FIDGET] = LAYOUT(
+        KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      SWITCH_TO_NEXT_LAYER,
+        KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,
+        KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,
+        KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO
+    )
+};
+
+// State variables
+uint8_t current_key_layer_index_user = 0;
+
+
+/*
+ * Moves directly to the specified layer
+ */
+void go_to_key_layer_user(uint8_t go_to_layer){
+
+    // Setup layer
+    switch (go_to_layer) {
+        case _PROGRAMMING:
+            break;
+        case _FIDGET:
+            break;
+    }
+
+    layer_move(go_to_layer);
+}
+
+/*
+ * Moves directly to the next layer, loops back
+ */ 
+void go_to_next_key_layer_user(void){
+    current_key_layer_index_user = (uint8_t) ((current_key_layer_index_user + 1) % LAYER_COUNT);
+    uint8_t next_layer = layer_list_user[current_key_layer_index_user];
+
+    go_to_key_layer_user(next_layer);
+}
+
+/*
+ * Process custom key code events
+ */ 
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+
+    switch (keycode) {
+        case SWITCH_TO_NEXT_LAYER:
+            // Switch to next layer
+            if (!(record->event.pressed)) {
+                // Key Up
+                go_to_next_key_layer_user();
+            }
+            break;
+    }
+
+    return true;
+}
+
+/*
+ * Initialization after keyboard finishes startup
+ */
+void keyboard_post_init_user(void) {
+    go_to_key_layer_user(layer_list_user[0]);
+}
---
+++ keyboards/koolertron/amag23/keymaps/kevinta893burnout/readme.md
@@ -0,0 +1,5 @@
+# A personal keymap for amag23
+
+Busted my keyboard due to controller/LED burnout. This keymap turns off all RGB and lighting features :(
+
+Remember to also turn off 'RGBLIGHT_ENABLE' in the rules.mk file
