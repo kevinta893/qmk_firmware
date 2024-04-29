735e72688720efd4d073024c7659ae7bfe08fd28
kevinta893 <kevinta893@gmail.com>
2024-04-29 22:54:07 +0900

    Update rules.mk with send string and disable led

--- keyboards/amag23/rules.mk
+++ keyboards/amag23/rules.mk
@@ -7,4 +7,5 @@ EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
+RGBLIGHT_ENABLE = no       # Enable keyboard RGB underglow
+SEND_STRING_ENABLE = yes    # Enable send string function
